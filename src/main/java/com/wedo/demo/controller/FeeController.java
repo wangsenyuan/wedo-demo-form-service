0f374575dc087d8994094ac5000e59a6f40933f1
WangSenyuan <brucewang@maycur.com>
2023-08-20 16:22:06 +0800

    add logs

--- src/main/java/com/wedo/demo/controller/FeeController.java
+++ src/main/java/com/wedo/demo/controller/FeeController.java
@@ -2,16 +2,18 @@ package com.wedo.demo.controller;
 
 import com.wedo.demo.domain.fee.Fee;
 import com.wedo.demo.domain.fee.FeeContext;
-import com.wedo.demo.domain.fee.entity.FeeEntity;
 import com.wedo.demo.domain.fee.service.FeeServiceImpl;
 import com.wedo.demo.dto.common.R;
 import com.wedo.demo.dto.fee.FeeDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping({"/fee", "FEE"})
 public class FeeController {
+    private static final Logger logger = LoggerFactory.getLogger(FeeController.class);
     @Autowired
     private FeeServiceImpl feeService;
 
@@ -24,10 +26,16 @@ public class FeeController {
 
     @PostMapping("/save")
     public R<FeeDto> save(@RequestBody FeeDto dto) {
-        FeeDto res = feeService.factory(FeeContext.current(), dto.getId(), builder -> {
-            updateValues(dto, builder);
-        }, FeeDto::new);
-        return R.success(res);
+        try {
+            logger.debug("save {}", dto);
+            FeeDto res = feeService.factory(FeeContext.current(), dto.getId(), builder -> {
+                updateValues(dto, builder);
+            }, FeeDto::new);
+            return R.success(res);
+        } catch (RuntimeException ex) {
+            logger.warn("failed to save fee {}", dto, ex);
+            throw ex;
+        }
     }
 
     private static void updateValues(FeeDto dto, Fee.Builder builder) {
